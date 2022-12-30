83935eee1468b3129026e1fef874cedd6fb0e3dd
Arvo <arvofinland@gmail.com>
2022-12-30 21:59:45 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
 
+        int a = 100; //текущий счет
+        int b = 1500; //пополнение счета
+        int bonus;
+
+        if (b > 1000) {
+            System.out.println("Бонус:");
+            System.out.println(bonus = (b + a) / 100);
+            System.out.println("Итоговый счет:");
+            System.out.println((a + b) + bonus);
+
+
+        } else {
+            System.out.println("Бонус:");
+            System.out.println(0);
+            System.out.println("Итоговый счет:");
+            System.out.println(a + b);
+        }
     }
 }
