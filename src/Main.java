a7d73bc342b34c54b8698a7a171f70da1e4734cb
Arvo <arvofinland@gmail.com>
2023-01-09 21:54:40 +0300

    rename the variables data

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,22 @@
 public class Main {
     public static void main(String[] args) {
 
-        int a = 100; //текущий счет
-        int b = 1500; //пополнение счета
+        int mainCheck = 100; //текущий счет
+        int fillCheck = 1500; //пополнение счета
         int bonus;
 
-        if (b > 1000) {
+        if (fillCheck > 1000) {
             System.out.println("Бонус:");
-            System.out.println(bonus = (b + a) / 100);
+            System.out.println(bonus = (fillCheck + mainCheck) / 100);
             System.out.println("Итоговый счет:");
-            System.out.println((a + b) + bonus);
+            System.out.println((mainCheck + fillCheck) + bonus);
 
 
         } else {
             System.out.println("Бонус:");
             System.out.println(0);
             System.out.println("Итоговый счет:");
-            System.out.println(a + b);
+            System.out.println(mainCheck + fillCheck);
         }
     }
 }
